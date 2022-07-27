89c103be8e3bb2a9ecec562e72305902bbf390e7
Razvan <razvanccst@users.noreply.github.com>
2022-07-27 13:39:18 +0300

    Code refactoring adding .route()

--- app.js
+++ app.js
@@ -30,46 +30,86 @@ const Article = mongoose.model("Article", articleSchema);
 //HTTP VERBS -
 //REST GET (Get all Articles from DB)
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, articles) {
-    if (!err) {
-      res.send(articles);
-    } else {
-      res.send(err);
-    }
-  });
-});
+// app.get("/articles", function (req, res) {
+//   Article.find(function (err, articles) {
+//     if (!err) {
+//       res.send(articles);
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
 
-// REST POST - Via Postman
+// // REST POST - Via Postman
 
-app.post("/articles", function (req, res) {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
+// app.post("/articles", function (req, res) {
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content,
+//   });
 
-  newArticle.save(function (err) {
-    if (!err) {
-      res.send("Successfully added a new article");
-    } else {
-      res.send(err);
-    }
-  });
-});
+//   newArticle.save(function (err) {
+//     if (!err) {
+//       res.send("Successfully added a new article");
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
 
-// REST DELETE - Delete all articles - Via Postman
+// // REST DELETE - Delete all articles - Via Postman
 
-app.delete("/articles", function (req, res) {
-  Article.deleteMany(
-    /*{condition}*/ function (err) {
+// app.delete("/articles", function (req, res) {
+//   Article.deleteMany(
+//     /*{condition}*/ function (err) {
+//       if (!err) {
+//         res.send("Successfully deleted all articles");
+//       } else {
+//         res.send(err);
+//       }
+//     }
+//   );
+// });
+
+// ADDING ROUTE - Code Refactoring adding route
+// app.route("/articles").get().post().delete();
+
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, articles) {
       if (!err) {
-        res.send("Successfully deleted all articles");
+        res.send(articles);
       } else {
         res.send(err);
       }
-    }
-  );
-});
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(
+      /*{condition}*/ function (err) {
+        if (!err) {
+          res.send("Successfully deleted all articles");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
